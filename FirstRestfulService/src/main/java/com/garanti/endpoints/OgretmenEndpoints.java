4417db54e9f9fefb10693d01b37a7f887a5e6fe2
Emre Çelen <79768707+emrcelen@users.noreply.github.com>
2022-12-27 18:03:00 +0300

    first-commit

---
+++ FirstRestfulService/src/main/java/com/garanti/endpoints/OgretmenEndpoints.java
@@ -0,0 +1,60 @@
+package com.garanti.endpoints;
+
+import com.garanti.model.Ogretmen;
+import com.garanti.repo.OgretmenRepo;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.ArrayList;
+
+@Path(value = "ogretmen")
+public class OgretmenEndpoints
+{
+    private OgretmenRepo repo;
+
+    public OgretmenEndpoints()
+    {
+        this.repo = new OgretmenRepo();
+    }
+
+    @GET
+    @Path(value = "getAll")
+    @Produces(value = MediaType.APPLICATION_JSON)
+    public ArrayList<Ogretmen> getAll()
+    {
+        // localhost:9090/FirstRestfulService/ogretmen/getAll
+        return repo.getAll();
+    }
+
+    @GET
+    @Path(value = "getById")
+    @Produces(value = MediaType.APPLICATION_JSON)
+    public Ogretmen getByIdQueryParam(@QueryParam(value = "id") Integer id)
+    {
+        // localhost:9090/FirstRestfulService/ogretmen/getById?id=1
+        return repo.getById(id);
+    }
+
+    @GET
+    @Path(value = "getById/{id}")
+    @Produces(value = MediaType.APPLICATION_JSON)
+    public Ogretmen getByIdPathParam(@PathParam(value = "id") Integer id)
+    {
+        // path param yazarsam bütün param 'lar set edilmiş olmak zorunda
+        // localhost:9090/FirstRestfulService/ogretmen/getById/1
+        return repo.getById(id);
+    }
+
+    @POST
+    @Path(value = "save")
+    @Consumes(value = MediaType.APPLICATION_JSON)
+    public String save(Ogretmen ogretmen)
+    {
+        // localhost:9090/FirstRestfulService/ogretmen/save
+        repo.save(ogretmen);
+        return "Başarı ile kaydedildi";
+    }
+
+    // localhost:9090/FirstRestfulService/ogretmen/deleteById
+}
