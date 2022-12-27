77e3093b43deae6782767d8b5d82602fb0fbfed8
Emre Çelen <79768707+emrcelen@users.noreply.github.com>
2022-12-27 18:19:20 +0300

    ders endpoint added

---
+++ FirstRestfulService/src/main/java/com/garanti/endpoints/DersEndpoints.java
@@ -0,0 +1,53 @@
+package com.garanti.endpoints;
+
+import com.garanti.model.Ders;
+import com.garanti.repo.DersRepo;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.ArrayList;
+
+@Path(value = "ders")
+public class DersEndpoints {
+    private DersRepo dersRepo;
+
+    public DersEndpoints(){
+        this.dersRepo = new DersRepo();
+    }
+
+    // localhost:9090/FirstRestfulService/ders/getAll
+    @GET
+    @Path(value = "getAll")
+    @Produces(value = MediaType.APPLICATION_JSON)
+    public ArrayList<Ders> getAll(){
+        return this.dersRepo.getAll();
+    }
+    // localhost:9090/FirstRestfulService/ders/getById/id
+    @GET
+    @Path(value = "getById/{id}")
+    @Produces(value = MediaType.APPLICATION_JSON)
+    public Ders getByPathId(@PathParam("id") int id){
+        return this.dersRepo.getById(id);
+    }
+    // localhost:9090/FirstRestfulService/ders/getById?id=id
+    @GET
+    @Path(value = "getById")
+    @Produces(value = MediaType.APPLICATION_JSON)
+    public Ders getByQueryId(@QueryParam("id") int id){
+        return this.dersRepo.getById(id);
+    }
+    // localhost:9090/FirstRestfulService/ders/save
+    @POST
+    @Path(value = "save")
+    @Consumes(value = MediaType.APPLICATION_JSON)
+    public String save(Ders ders){
+        this.dersRepo.save(ders);
+        return "Başarılı bir şekilde ders kaydı gerçekleşti.";
+    }
+}
