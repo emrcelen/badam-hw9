be9581d6469d416c8a38a9e28a49b258c0297f3d
Emre Çelen <79768707+emrcelen@users.noreply.github.com>
2022-12-28 19:19:47 +0300

    The ogretmen endpoint has been updated.

--- FirstRestfulService/src/main/java/com/garanti/endpoints/OgretmenEndpoints.java
+++ FirstRestfulService/src/main/java/com/garanti/endpoints/OgretmenEndpoints.java
@@ -2,10 +2,14 @@ package com.garanti.endpoints;
 
 import com.garanti.model.Ogretmen;
 import com.garanti.repo.OgretmenRepo;
+import jakarta.json.Json;
+import jakarta.json.JsonReader;
+import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
+import java.io.StringReader;
 import java.util.ArrayList;
 
 @Path(value = "ogretmen")
@@ -30,7 +34,7 @@ public class OgretmenEndpoints
     @GET
     @Path(value = "getById")
     @Produces(value = MediaType.APPLICATION_JSON)
-    public Ogretmen getByIdQueryParam(@QueryParam(value = "id") Integer id)
+    public Ogretmen getByIdQueryParam(@QueryParam(value = "id")  Integer id)
     {
         // localhost:9090/FirstRestfulService/ogretmen/getById?id=1
         return repo.getById(id);
@@ -46,6 +50,13 @@ public class OgretmenEndpoints
         return repo.getById(id);
     }
 
+    @GET
+    @Path(value = "getByHeaderId")
+    @Produces(value = MediaType.APPLICATION_JSON)
+    public Ogretmen getByIdHeaderParam(@HeaderParam(value = "id")int id){
+        return repo.getById(id);
+    }
+
     @POST
     @Path(value = "save")
     @Consumes(value = MediaType.APPLICATION_JSON)
@@ -56,5 +67,21 @@ public class OgretmenEndpoints
         return "Başarı ile kaydedildi";
     }
 
-    // localhost:9090/FirstRestfulService/ogretmen/deleteById
+    // localhost:9090/FirstRestfulService/ogretmen/deleteById/id
+    @DELETE
+    @Path(value = "deleteById/{id}")
+    public String deleteById(@PathParam(value = "id") int id){
+        if(repo.deleteById(id))
+            return "Başarılı bir şekilde silme işlemini gerçekleştirdi.";
+        return "Silme işlemi gerçekleştirilemedi.";
+    }
+    // localhost:9090/FirstRestfulService/ogretmen/deleteById/
+    @DELETE
+    @Path(value = "deleteById")
+    public String deleteByHeaderId(@HeaderParam(value = "id") int id){
+        if(repo.deleteById(id))
+            return "Başarılı bir şekilde silme işlemini gerçekleştirdi.";
+        return "Silme işlemi gerçekleştirilemedi.";
+    }
+
 }
